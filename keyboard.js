8bea5a2c79c2988ccfc818a8b37b2f45a74a9ff3
gwlgwlgel <83810740+gwlgwlgel@users.noreply.github.com>
2021-05-07 11:37:01 +0300

    Delete keyboard.js

--- keyboard.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('keydown', function (e) {
-    window.keyPressed = e.key;
-}, false);
-
-var keyboard = {
-    button: function() {
-        return window.keyPressed;
-    }
-}
